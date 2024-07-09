0acc00248682ec3dbe5f32faf761f0f35c990766
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2024-07-09 11:40:19 +0530

    request handling part i

---
+++ UI/app.js
@@ -0,0 +1,22 @@
+Dropzone.autoDiscover = false;
+
+function init() {
+    let dz = new Dropzone("#dropzone", {
+        url: "/",
+        maxFiles: 1,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Some Message",
+        autoProcessQueue: false
+    });
+    
+    dz.on("addedfile", function() {
+        if (dz.files[1]!=null) {
+            dz.removeFile(dz.files[0]);        
+        }
+    });
+
+    dz.on("complete", function (file) {
+        let imageData = file.dataURL;
+        
+        var url = "http://127.0.0.1:5000/classify_image";
+
